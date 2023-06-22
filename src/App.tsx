170d05a6f60bb11fa551ba16e7480d6fde9e8bd4
Leonardo Minora <leonardo.minora@gmail.com>
2023-06-22 08:24:45 -0300

    Adicionado AppTarefas com botão e lista

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,40 @@ const AppNavBar = () => {
 		</div>
 	);
 };
+
+const AppTarefas = () => {
+	return (
+		<div className="card">
+			<button>Pegar tarefas</button>
+			<ul>
+				<li>
+					Criar o projeto, adicionar bibliotecas e limpar o código
+				</li>
+				<li>
+					Criar o componente AppNavBar com o título da aplicação e
+					adicionar instância em App
+				</li>
+				<li>
+					Criar o componente AppTarefas com uma lista de tarefas e
+					adicionar instância em App
+				</li>
+				<li>
+					Transferir dados da lista para estado tarefas de AppTarefas
+				</li>
+				<li>
+					Montas o estado tarefas do componente AppTarefas a partir de
+					requisião a API
+				</li>
+			</ul>
+		</div>
+	);
+};
+
 const App = () => {
 	return (
 		<>
 			<AppNavBar />
+			<AppTarefas />
 		</>
 	);
 };
