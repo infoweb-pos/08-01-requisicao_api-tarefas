da49aa12509977f1a2ec708d6a435290139421bb
Leonardo Minora <leonardo.minora@gmail.com>
2023-06-22 08:19:19 -0300

    Adicionado AppNavBar com um título do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
+const AppNavBar = () => {
+	return (
+		<div className="card">
+			<h1>Lista de tarefas (apenas leitura)</h1>
+		</div>
+	);
+};
 const App = () => {
-	return <></>;
+	return (
+		<>
+			<AppNavBar />
+		</>
+	);
 };
 
 export default App;
